96d83d5060e7d0438d9aee260bb6d92e6152b789
vikassssssssssss <vikassuthar049@gmail.com>
2025-01-08 10:31:55 +0530

    todays code

--- conditions.js
+++ conditions.js
@@ -74,14 +74,14 @@
 //let b =140;
 //let c =100;
 // do this with both ifelse and switch condition
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
-    }
-  }
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
---
+++ looping.js
@@ -0,0 +1,37 @@
+//! for loop :
+// for(let  i=1;i<=5;i++){                 //i=iteration
+// console.log("Good morning")
+// }
+// let n=100;
+// let x=prompt("enter an number");
+// for(let i=0;i<=n;i++){
+
+//     if(1%2==0){
+//         console.log(i + " is a even number");
+//     }
+//     else{
+//         console.log(i + "is an odd number")
+//     }
+// }
+
+// let i=1;
+// while(i<=5){
+//     console.log("Good morning");
+//     i++;                                         //initializer must be outside and updater should be givin (i++)
+// }
+
+//! do while
+let n=5; 
+do{
+    i++
+    console.log("very good ")
+}while(i<=n)
+
+// function add to cart(){
+//     alert("item added to cart");
+// }
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(10,20);
