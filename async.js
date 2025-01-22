d7be93af065aa3e90686f33d2d82120da72c9929
vikassssssssssss <vikassuthar049@gmail.com>
2025-01-22 18:10:41 +0530

    tokay codeeee

--- async.js
+++ async.js
@@ -16,14 +16,14 @@
 //     console.log(10);
 
 
-let p2 = new Promise((resolve, reject)=>{
-    resolve("success");
-})
-console.log(p2);
-p2
-.then((response)=>console.log(response))
-p2.catch((error)=>console.log(error))
-.finally(() => console.log("finally for both"));
+// let p2 = new Promise((resolve, reject)=>{
+//     resolve("success");
+// })
+// console.log(p2);
+// p2
+// .then((response)=>console.log(response))
+// p2.catch((error)=>console.log(error))
+// .finally(() => console.log("finally for both"));
 
 // let p3 = new Promise((resolve,reject)=>{
 //     reject("Failure");
@@ -32,6 +32,36 @@ p2.catch((error)=>console.log(error))
 // // console.log(p3);
 
 // p3.then(data) => console.log
+
+//! API fetching 
+
+//! https://jsonplaceholder.typicode.com/users
     
+function fetchUsers()
+{
+    let response = fetch("https://jsonplaceholder.typicode.com/users");
+    // console.log(response);
+    response.then(result=>{
+        // console.log(result.json());
+        return result.json().then(data=>{
+            console.log(data);
+            let store = document.getElementById("store");
+            console.log(store);
+            data.map(user=>{
+                // console.log(user);
+                store.innerHTML += `
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.Name}</td>
+                </tr>
+                `
+            })
+        })
+    })
+    .catch(err=>console.log(err))
+}
+fetchUsers();
 
 
