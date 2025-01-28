42c44bb88da38f5dcec1222bcab8464284240c54
vikassssssssssss <vikassuthar049@gmail.com>
2025-01-28 17:57:41 +0530

    dom code and dragdrop

--- dom.js
+++ dom.js
@@ -109,21 +109,132 @@
 // document.body.appendChild(ele);
 // document.body.appendChild(image);
 
-let from = document.querySelector("form");
-let username = document.getElementById("uName");
-let email = document.getElementById("uEmail");
-let password = document.getElementById("uPass");
+// let from = document.querySelector("form");
+// let username = document.getElementById("uName");
+// let email = document.getElementById("uEmail");
+// let password = document.getElementById("uPass");
 
-from.addEventListener("submit",(Event)=>{
+// from.addEventListener("submit",(Event)=>{
+//     event.preventDefault();
+//     // console.log(event);
+//     // console.log("form submitted");
+//     let uName = username.value;
+//     let uEmail = email.value;
+//     let uPass = password.value;
+//     let userDetails={
+//         uName,uEmail,uPass
+//     }
+//     console.log(userDetails);
+//     localStorage.setItem("userData",JSON.stringify(userDetails))
+// })
+
+// // localStorage.clear();
+
+// ! 
+
+// let vd= document.createElement("video");
+// vd.setAttribute("src" ,"./video123.mp4");
+// vd.setAttribute("controls" , "controls");
+// console.log(vd);
+
+
+// document.body.appendChild(vd)
+
+// let h1 = document.createElement("h1");
+// h1.setAttribute("id","demo")
+// h1.innerText = "MRU Batch3 JS session";
+// console.log(h1);
+// h1.removeAttribute("id");
+
+// document.body.appendChild(h1);
+
+
+// ! 
+// let mainEle = document.createElement("div");
+// mainEle.setAttribute("id","mainBlock");
+// mainEle.style.border="2px solid red";
+// mainEle.style.width="550px";
+// mainEle.style.height="550px";
+
+// console.log(mainEle);
+
+
+// let topEle = document.createElement("div");
+// topEle.setAttribute("class","topBlock");
+// // console.log(topEle);
+
+// let image = document.createElement("img");
+// image.src="https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
+// image.style.width="550px"
+
+// let bottomEle = document.createElement("div");
+// bottomEle.setAttribute("class","bottomBlock");
+// // console.log(bottomEle);
+
+// let h1 = document.createElement("h1");
+// h1.innerText = "CAT";
+// h1.style.textAlign="center"
+
+// let btn = document.createElement("button");
+// btn.innerText="View More";
+// btn.style.border="none";
+// btn.style.padding="20px";
+// btn.style.backgroundColor="dodgerblue";
+// btn.style.color="white";
+
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+
+// topEle.appendChild(image);
+// mainEle.appendChild(topEle);
+// mainEle.appendChild(bottomEle);
+// document.body.appendChild(mainEle);
+
+//!
+
+let form = document.querySelector("form");
+
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show");
+
+let gender = document.getElementsByName("gender");
+// console.log(gender);    
+
+//! hiding and showing the password values
+check.addEventListener("click",event=>{
+    // console.log(event.target.checked);
+    if(event.target.checked == true){
+        password.setAttribute("type","text");
+        show.innerText = "hide password";
+    }else{
+        password.setAttribute("type","password");
+        show.innerText = "show password";
+    }
+})
+
+form.addEventListener("submit" , event=>{
     event.preventDefault();
-    // console.log(event);
-    // console.log("form submitted");
-    let uName = username.value;
-    let uEmail = email.value;
-    let uPass = password.value;
+    let un = username.value;
+    let up = password.value;
+    let gen = "";
+
+    for(let i= 0;i<=gender.length-1;i++){
+        // console.log(gender[i].value);
+        // console.log(gender[i].checked);
+        if(gender[i].checked == true){
+            // console.log(gender[i].value);
+            gen = gender[i].value;
+        }
+
+    }
+    // console.log(un , up , gen);
     let userDetails={
-        uName,uEmail,uPass
+        username:un,
+        password:up,
+        gender:gen
     }
     console.log(userDetails);
-    localStorage.setItem("userData",JSON.stringify(userDetails))
+    sessionStorage.setItem("userData", JSON.stringify(userDetails))
 })
